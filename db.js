18071c7fd560a7216534b7f76e917f214a501730
cesarpgs <cesarpgasuz@gmail.com>
2022-08-06 11:07:03 -0500

    update sin valor

---
+++ db.js
@@ -0,0 +1,45 @@
+const negocios = [
+
+	{
+		id: 'neg01',
+		nombre: 'tienda 1',
+		direccion: 'primera direccion',
+		telefono: '419'
+	},
+
+	{
+		id: 'neg02',
+		nombre: 'tienda 2',
+		direccion: 'segunda direccion',
+		telefono: '4222',
+		servicios: {
+			servicio01 : {
+						nombre01: 'papeleria',
+						nombre02: 'imprimir hojas',
+						nombre03: 'cortar hojas',
+				},
+			servicio02 : {
+						nombre01: 'informatica',
+						nombre02: 'formatear',
+						nombre03: 'hackear',
+				},
+
+			},
+			
+		},
+	
+	
+
+
+	{
+		id: 'neg03',
+		nombre: 'tienda 2',
+		direccion: 'tercera direccion',
+		telefono: '112'
+	}
+
+
+
+
+
+]
