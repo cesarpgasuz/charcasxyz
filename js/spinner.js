709e149ac6585f8c53606ae32a8597e7b011627d
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2022-08-28 00:45:22 -0500

    spinner udpate
    otra posible solucion al spinner

--- js/spinner.js
+++ js/spinner.js
@@ -1,3 +1,6 @@
+
+
+
 const botonMenu = [...document.querySelectorAll('.boton-menu')];
 botonMenu.forEach(boton => {
     console.log(boton)
@@ -6,18 +9,56 @@ botonMenu.forEach(boton => {
 
 
 const abrirSpinner = [...document.querySelectorAll('.visitar')];
+const spinner = document.querySelector('.spinner');
 
 
 
 for(const irse of abrirSpinner){
     
     irse.addEventListener('click', () => {
-        const spinner = document.createElement('div');
-        spinner.classList.add('spinner');
-        document.body.insertBefore(spinner, document.querySelector('.navv'));
+        spinner.classList.remove('oculto');
+        spinner.classList.add('activo');
     })
 
 }
+
+
+//document.addEventListener('DOMContentLoaded', ocultarSpinner);
+
+function ocultarSpinner(){
+    if(spinner.classList.contains('activo')){
+        spinner.style.display = 'none';
+    }
+}
+
+
+if (document.readyState == 'interactive') {
+  // cargando todavía, esperar el evento
+  console.log('todavia no se lee')
+  document.addEventListener('DOMContentLoaded', ocultarSpinner);
+  
+  //document.addEventListener('DOMContentLoaded', work);
+} else if(document.readyState == 'loading'){
+    console.log('cargando..')
+    spinner.style.display = 'none';
+  
+}else{
+    // DOM está listo!
+  console.log('leido')
+  spinner.style.display = 'none';
+
+}
+
+
+
+
+
+/*
+window.addEventListener('unload', () => {
+    spinner.remove();
+})
+
+*/
 
 
 
