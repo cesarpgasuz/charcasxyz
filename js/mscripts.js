7a8f7983811850e276c5c1d8378c9c4b67b5d770
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2022-08-29 18:16:56 -0500

    Update mscripts.js

--- js/mscripts.js
+++ js/mscripts.js
@@ -1,25 +1,20 @@
-////////////////////////////////////////////////////////////////////////////
+
 const hambur = document.querySelector('#hambur');
 const sombra = document.querySelector('.sombra');
 const menuNav = document.querySelector('.menu-nav');
 
-hambur.addEventListener('click', () => {
- 		
- 	menuNav.classList.toggle('visibles');
- 	document.body.classList.toggle('scroll');
- 	sombra.classList.toggle('visible-som');
-
- })
+hambur.addEventListener('click', () => {	
+ menuNav.classList.toggle('visibles');
+ document.body.classList.toggle('scroll');
+ sombra.classList.toggle('visible-som');
+});
 
 sombra.addEventListener('click', () => {
+ menuNav.classList.remove('visibles');
+ document.body.classList.remove('scroll');
+ sombra.classList.remove('visible-som');
+});
 
- 	menuNav.classList.remove('visibles');
- 	document.body.classList.remove('scroll');
- 	sombra.classList.remove('visible-som');
-
-})
-
-/////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -59,7 +54,7 @@ observador.observe(banner);
 
 
 
-///////////////////////////////////////////////////////////////////////////////
+
 
 document.querySelector('#horario').addEventListener('click', (e) => {
 	document.querySelector(".modall").classList.toggle("visible");
@@ -80,7 +75,7 @@ document.querySelector('#closeH').addEventListener('click', (e) => {
 });
 
 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 const botonMenu = [...document.querySelectorAll('.boton-menu')];
 botonMenu.forEach(boton => {
