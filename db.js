26fb50cc75e2e658b9f0c8437b7192940f28c3c9
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2023-01-16 16:38:42 -0600

    nuevo script para los datos personales
    se agrego un script para obtener los datos de los negocios de forma dinamica

--- db.js
+++ db.js
@@ -1,89 +1,121 @@
-const negocios = [
 
-	{
-		id: 'neg01',
-		nombre: 'tienda 1',
-		direccion: 'primera direccion',
-		telefono: '419'
-	},
-/*
-	{
-		id: 'neg02',
-		nombre: 'tienda 2',
-		direccion: 'segunda direccion',
-		telefono: '4222',
-		whatsapp: '4191389032',
-		servicios: [
-			{
-				id: 'papeleria',
-				tareas : [{nombre: 'cortar papel', id: 'papeleria'}, {nombre: 'triturar hojas', id: 'papleria'}, {nombre: 'colorear hojas', id: 'papeleria'} ]
-			},
-			{
-				id: 'celular',
-				tareas : [{nombre: 'reparar celular', id: 'celular'}, {nombre: 'pegar celular', id: 'celular'}, {nombre: 'entregar', id: 'celular'} ]
-			},
+const db = [
 
-		]
-			
-		},
-	
-
-*/
-	{
-		id: 'neg02',
-		nombre: 'tienda 2',
-		direccion: 'segunda direccion',
-		telefono: '4222',
-		whatsapp: '4191389032',
-		servicios: [
-			{
-				id: 'papeleria',
-				tarea: ['cortar papel', 'pegar papel', 'triturar papel', 'colorear hojas', 'pintar hojas', 'pagina hojas' ],
-			},
-			{
-				id: 'celular',
-				tarea: ['recargar celular', 'romper celular', 'limpiar celular' ],
-			},
-			{
-				id: 'series',
-				tarea: ['pagar mensualidad', 'contratar servicio' ],
-			},
-			{
-				id: 'peliculas',
-				tarea: ['comprar dvd', 'reproducir dvd', 'guardar dvd' ],
-			},
-
-		]
-			
-		},
-	
-
-
-	{
-		id: 'neg03',
-		nombre: 'tienda 3',
-		direccion: 'tercera direccion',
-		telefono: '112',
-		whatsapp: 'xxx',
-		servicios: [
-			{
-				id: 'xxx',
-				tarea: ['jaja', 'meee', 'cuas' ],
-			},
-			{
-				id: 'xx',
-				tarea: ['vbv', 'vvvvv', 'ssss' ],
-			},
-			{
-				id: 'xxxd',
-				tarea: ['ttt', 'tttt', 'tttt' ],
-			},
-
-		]
-	}
-
-
-
-
+    {   id: 'ele1',
+        nombre: 'Electronica Sanchez',
+        direccion: 'Calle San luis de la Paz #24, Doctor Mora',
+        extracto: 'Ventas de Equipos y Refacciones Electrónicas para Sonidos, Luces, TV, MP3, Computadoras, Etc.',
+        telefono: '4681231545',
+        whatsapp: '4681231545',
+        facebook: 'electronicasanchezdoctormora',
+        imagen: 'electronica-sanchez.jpg'
+    },
+    {   id: 'ele2',
+        nombre: 'Dyxer Team',
+        direccion: 'Av. Guanajuato #18',
+        extracto: 'Diseño de paginas web',
+        telefono: '4191389032',
+        whatsapp: '4191389032',
+        facebook: 'quizas92',
+        imagen: 'logo.jpg'
+    }
 
 ]
+           
+       
+
+
+
+
+
+
+
+// const negocios = [
+
+// 	{
+// 		id: 'neg01',
+// 		nombre: 'tienda 1',
+// 		direccion: 'primera direccion',
+// 		telefono: '419'
+// 	},
+// /*
+// 	{
+// 		id: 'neg02',
+// 		nombre: 'tienda 2',
+// 		direccion: 'segunda direccion',
+// 		telefono: '4222',
+// 		whatsapp: '4191389032',
+// 		servicios: [
+// 			{
+// 				id: 'papeleria',
+// 				tareas : [{nombre: 'cortar papel', id: 'papeleria'}, {nombre: 'triturar hojas', id: 'papleria'}, {nombre: 'colorear hojas', id: 'papeleria'} ]
+// 			},
+// 			{
+// 				id: 'celular',
+// 				tareas : [{nombre: 'reparar celular', id: 'celular'}, {nombre: 'pegar celular', id: 'celular'}, {nombre: 'entregar', id: 'celular'} ]
+// 			},
+
+// 		]
+			
+// 		},
+	
+
+// */
+// 	{
+// 		id: 'neg02',
+// 		nombre: 'tienda 2',
+// 		direccion: 'segunda direccion',
+// 		telefono: '4222',
+// 		whatsapp: '4191389032',
+// 		servicios: [
+// 			{
+// 				id: 'papeleria',
+// 				tarea: ['cortar papel', 'pegar papel', 'triturar papel', 'colorear hojas', 'pintar hojas', 'pagina hojas' ],
+// 			},
+// 			{
+// 				id: 'celular',
+// 				tarea: ['recargar celular', 'romper celular', 'limpiar celular' ],
+// 			},
+// 			{
+// 				id: 'series',
+// 				tarea: ['pagar mensualidad', 'contratar servicio' ],
+// 			},
+// 			{
+// 				id: 'peliculas',
+// 				tarea: ['comprar dvd', 'reproducir dvd', 'guardar dvd' ],
+// 			},
+
+// 		]
+			
+// 		},
+	
+
+
+// 	{
+// 		id: 'neg03',
+// 		nombre: 'tienda 3',
+// 		direccion: 'tercera direccion',
+// 		telefono: '112',
+// 		whatsapp: 'xxx',
+// 		servicios: [
+// 			{
+// 				id: 'xxx',
+// 				tarea: ['jaja', 'meee', 'cuas' ],
+// 			},
+// 			{
+// 				id: 'xx',
+// 				tarea: ['vbv', 'vvvvv', 'ssss' ],
+// 			},
+// 			{
+// 				id: 'xxxd',
+// 				tarea: ['ttt', 'tttt', 'tttt' ],
+// 			},
+
+// 		]
+// 	}
+
+
+
+
+
+// ]
