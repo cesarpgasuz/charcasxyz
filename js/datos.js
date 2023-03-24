1acc11af70f054eb484c4267cd019250954b0aaf
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2023-03-23 21:45:05 -0600

    filtro y datos type module
    se agrego el filtro y los datos por js, se agrego un nuevo negocio a la categoria taxis

--- js/datos.js
+++ js/datos.js
@@ -1,4 +1,11 @@
-document.addEventListener('DOMContentLoaded', function(){
+import {db} from '../db.js';
+import {formatearNumero} from './formatear.js';
+
+
+
+iniciarApp();
+
+function iniciarApp(){
 
     //variables
     const id = document.querySelector('body').dataset.id;
@@ -108,17 +115,7 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
 
-    function formatearNumero(numero){
-
-        const numeroAntiguo = numero;
-        const sub1 = numeroAntiguo.substring(0,3);
-        const sub2 = numeroAntiguo.substring(3,6);
-        const sub3 = numeroAntiguo.substring(6,8);
-        const sub4 = numeroAntiguo.substring(8,10);
-        const numeroNuevo = [sub1,sub2,sub3,sub4].join(' ');
-
-        return numeroNuevo;
-    }
+    
        
         // agregamos al html
 
@@ -128,9 +125,7 @@ document.addEventListener('DOMContentLoaded', function(){
         contenedor.appendChild(informationDiv);
 
 
-
-});
-
+};
 
 
 
